360a59e7cc0f63ada4c3e21fca9f72c84ebf7beb
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-27 21:48:19 -0300

    criando a classe ClienSocialNetwork em models.py

--- projetoapp/apps/clients/models.py
+++ projetoapp/apps/clients/models.py
@@ -23,4 +23,15 @@ class Client(models.Model):
 
     def __str__(self):
         return self.first_name
+class ClientSocialNetwork(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    socialNetwork = models.ForeignKey(SocialNetwork, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Item de Redes Social'
+        verbose_name_plural = 'Itens de Rede Social'
+        ordering =['id']
+
+    def __str__(self):
+        return self.socialNetwork.name 
 
