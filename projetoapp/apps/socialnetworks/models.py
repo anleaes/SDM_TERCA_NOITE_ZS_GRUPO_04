e141a4ca6d01520356a2dfd480e2ceaf64efde2d
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-27 23:11:48 -0300

    criando a classe socialnetwork

--- projetoapp/apps/socialnetworks/models.py
+++ projetoapp/apps/socialnetworks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class SocialNetwork(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    
