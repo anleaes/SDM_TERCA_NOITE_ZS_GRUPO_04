972ccc72f42a0476a93d3fe65dace0fbcf4d016c
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-27 23:12:21 -0300

    criando a classe Meta

--- projetoapp/apps/socialnetworks/models.py
+++ projetoapp/apps/socialnetworks/models.py
@@ -4,4 +4,11 @@ from django.db import models
 class SocialNetwork(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
-    
+    class Meta:
+        verbose_name = 'Rede Social'
+        verbose_name_plural = 'Redes Sociais'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+
