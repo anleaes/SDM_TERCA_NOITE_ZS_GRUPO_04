43a8c45e1ec19a59c290f4382b00a25c004112bc
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-27 23:22:41 -0300

    adicionando imports em views.py de socialnetworks

--- projetoapp/apps/socialnetworks/views.py
+++ projetoapp/apps/socialnetworks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import SocialNetwork
+from rest_framework import viewsets
+from .serializer import SocialNetworkSerializer
 
 # Create your views here.
