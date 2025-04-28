fb6eff00ed869e1ad45b8cb627e47289ce735fc3
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-28 00:19:15 -0300

    adicionando class Meta

--- projetoapp/apps/clientsocialnetwork/models.py
+++ projetoapp/apps/clientsocialnetwork/models.py
@@ -5,3 +5,10 @@ from socialnetworks.models import Socialnetwork
 class ClientSocialnetwork(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     socialnetwork = models.ForeignKey(Socialnetwork, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Rede Social do Cliente'
+        verbose_name_plural = 'Redes Sociais dos Clientes'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.client.first_name} - {self.socialnetwork.name}"
