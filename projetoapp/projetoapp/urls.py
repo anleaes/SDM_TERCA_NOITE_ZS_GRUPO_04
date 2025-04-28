6c3f55bda219ac9c77381b571cefa3ca19351c1c
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-27 23:33:21 -0300

    adicionando redessociais a urlspatterns em urls.py na raiz do projeto

--- projetoapp/projetoapp/urls.py
+++ projetoapp/projetoapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('redessocias/', include('socialnetworks.urls', namespace='redessociais')),    
     path('clientes/', include('clients.urls', namespace='clients')),
     path('clientes_redessociais/', include('clients.urls', namespace='clients_socialnetworks')),
     path('categorias/', include('categories.urls', namespace='categories')),
