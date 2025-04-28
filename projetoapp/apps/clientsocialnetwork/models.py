1e465318276b916a18fe9d167abecf945eb06cf5
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-28 00:17:56 -0300

    criando a classe clientsocialnetwork

--- projetoapp/apps/clientsocialnetwork/models.py
+++ projetoapp/apps/clientsocialnetwork/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from clients.models import Client
 from socialnetworks.models import Socialnetwork
 # Create your models here.
+class ClientSocialnetwork(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    socialnetwork = models.ForeignKey(Socialnetwork, on_delete=models.CASCADE)
