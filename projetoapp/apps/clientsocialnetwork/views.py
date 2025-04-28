c449cee85c5a32e946f478590cc3a030d73b8858
MarcosCRosa <marcosluiz4012@gmail.com>
2025-04-28 00:35:02 -0300

    criando a classe ClientSocialnetworkViewSet em views.py

--- projetoapp/apps/clientsocialnetwork/views.py
+++ projetoapp/apps/clientsocialnetwork/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import ClientSocialnetwork
 from .serializer import ClientSocialnetworkSerializer
-
 # Create your views here.
+class ClientSocialnetworkViewSet(viewsets.ModelViewSet):
+    queryset = ClientSocialnetwork.objects.all()
+    serializer_class = ClientSocialnetworkSerializer
