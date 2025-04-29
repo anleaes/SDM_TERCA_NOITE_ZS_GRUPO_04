ef1497d8083c4386b7168b8fe06cdee3fae32b65
lvsmendes <lvsmendes2@gmail.com>
2025-04-29 17:43:42 -0300

    criado o modelo orderitem

--- projetoapp/apps/orderitem/models.py
+++ projetoapp/apps/orderitem/models.py
@@ -4,3 +4,13 @@ from django.db import models
 from django.db import models
 from apps.orders.models import Order  
 from apps.products.models import Product 
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def verifica_total(self):
+        total = self._unitary_price * self._quantity
+        return total
