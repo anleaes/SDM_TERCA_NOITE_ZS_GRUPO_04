9c01402c2a0df7b788d997c46776df809eade1bf
lvsmendes <lvsmendes2@gmail.com>
2025-04-29 18:05:44 -0300

    regras especificas de quantidade e preco maior que zero adicionadas

--- projetoapp/apps/orderitem/serializer.py
+++ projetoapp/apps/orderitem/serializer.py
@@ -5,3 +5,13 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ['id', 'order', 'product', 'quantity', 'price']
+
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("A quantidade deve ser maior que zero.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("O preço unitário deve ser maior que zero.")
+        return value
