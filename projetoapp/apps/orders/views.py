090025115de121f1d15c29bd8570b078edf1751a
lvsmendes <lvsmendes2@gmail.com>
2025-04-28 21:38:06 -0300

    criada as views em views.py

--- projetoapp/apps/orders/views.py
+++ projetoapp/apps/orders/views.py
@@ -4,3 +4,10 @@ from rest_framework import viewsets
 from .serializer import OrderSerializer, OrderItemSerializer
 
 # Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer  
+    
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
