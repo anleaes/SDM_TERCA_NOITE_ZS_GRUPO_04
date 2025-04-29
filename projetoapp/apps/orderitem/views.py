5d9dfc3fa5899156c4bff10c164b82413e59302b
lvsmendes <lvsmendes2@gmail.com>
2025-04-29 18:19:28 -0300

    integracao do viewset as rotas

--- projetoapp/apps/orderitem/views.py
+++ projetoapp/apps/orderitem/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import OrderItem
 from .serializers import OrderItemSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
