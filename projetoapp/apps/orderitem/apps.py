443a867d7bb0e2e39f4ce50179c402b1b0a5cee3
lvsmendes <lvsmendes2@gmail.com>
2025-04-29 17:18:51 -0300

    criado o nome de identificaçao do aplicativo

--- projetoapp/apps/orderitem/apps.py
+++ projetoapp/apps/orderitem/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OrderitemConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orderitem"
+    verbose_name = 'Gestao de Pedidos'
