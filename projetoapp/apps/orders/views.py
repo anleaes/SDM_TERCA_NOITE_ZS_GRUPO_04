3d332d12707a6d4eda94e3fccc69a0ffb302bf74
lvsmendes <lvsmendes2@gmail.com>
2025-04-28 21:25:42 -0300

    adicionando imports em views.py

--- projetoapp/apps/orders/views.py
+++ projetoapp/apps/orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Order, OrderItem
+from rest_framework import viewsets
+from .serializer import OrderSerializer, OrderItemSerializer
 
 # Create your views here.
