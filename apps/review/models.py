a3936065a1d76d376040b5dc8f74e6545dd759ba
lvsmendes <lvsmendes2@gmail.com>
2025-04-29 21:35:38 -0300

    criação inicial da model Review com os campos principais.

--- apps/review/models.py
+++ apps/review/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from apps.products.models import Product  
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
