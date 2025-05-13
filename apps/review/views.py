453bf9197c27d1841d9d327bff81a6658c17bd62
lvsmendes <lvsmendes2@gmail.com>
2025-05-13 19:34:29 -0300

    views criadas

--- apps/review/views.py
+++ apps/review/views.py
@@ -5,3 +5,7 @@ from rest_framework import viewsets
 from .models import Review
 from .serializer import ReviewSerializer
 
+class ReviewViewSet(viewsets.ModelViewSet):
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
