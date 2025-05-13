52e01ccd8ca099c218cadf33788e4818e048b392
lvsmendes <lvsmendes2@gmail.com>
2025-05-13 20:38:15 -0300

    mudancas feitas no router

--- apps/review/urls.py
+++ apps/review/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ReviewViewSet
 
 router = DefaultRouter()
-router.register(r'reviews', ReviewViewSet, basename='review')
+router.register('', ReviewViewSet)
 
 app_name = 'review'
 
